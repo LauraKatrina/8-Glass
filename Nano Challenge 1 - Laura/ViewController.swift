bec7272c91ddc5d48cd6bed0cb95685336773da2
Laura <laurakatrina.create@gmail.com>
2022-04-27 12:02:10 +0700

    Initial Commit

---
+++ Nano Challenge 1 - Laura/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nano Challenge 1 - Laura
+//
+//  Created by Laura Katrina on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
